7acc5f166e3108a672f5a05deb8d272905c943e2
Nathalia <35870624+Thalkas@users.noreply.github.com>
2026-07-15 03:43:00 +0200

    Create config.py

---
+++ auto-tmdb/config.py
@@ -0,0 +1,69 @@
+GENRES_MOVIES = {
+    "Akcja": 28,
+    "Animacja": 16,
+    "Dokumentalny": 99,
+    "Dramat": 18,
+    "Familijny": 10751,
+    "Fantasy": 14,
+    "Historyczny": 36,
+    "Horror": 27,
+    "Komedia": 35,
+    "Kryminał": 80,
+    "Muzyczny": 10402,
+    "Przygodowy": 12,
+    "Romans": 10749,
+    "Sci-Fi": 878,
+    "Tajemnica": 9648,
+    "Thriller": 53,
+    "Western": 37,
+    "Wojenne": 10752,
+}
+
+ADDITIONAL_GENRES_MOVIES = {
+    "Bollywood": 355622,
+    "Komedia Romantyczna": 9799,
+    "Thriller Szpiegowski": 250808,
+}
+
+GENRES_SERIES = {
+    "Akcji i Przygodowe": 10759,
+    "Animacja": 16,
+    "Dokumentalny": 99,
+    "Dramat": 18,
+    "Dziecięcy": 10762,
+    "Familijny": 10751,
+    "Komedia": 35,
+    "Kryminał": 80,
+    "Sci-Fi i Fantasy": 10765,
+    "Tajemnica": 9648,
+    "Thriller": 53,
+    "Western": 37,
+    "Wojenne i Polityczne": 10768,
+}
+
+ADDITIONAL_GENRES_SERIES = {
+    "Historyczny": 36,
+    "Horror": 3133,
+    "Muzyczny": 10402,
+    "Romans": 9840,
+}
+
+PROVIDERS = {
+    "Netflix": 8,
+    "HBO Max": 1899,
+    "Disney Plus": 337,
+    "Prime Video": 119,
+    "Apple TV": 350,
+    "SkyShowTime": 1773,
+}
+
+DECADES = {
+    "Lata 20": ("2020-01-01", "2029-12-31"),
+    "Lata 10": ("2010-01-01", "2019-12-31"),
+    "Lata 00": ("2000-01-01", "2009-12-31"),
+    "Lata 90": ("1990-01-01", "1999-12-31"),
+    "Lata 80": ("1980-01-01", "1989-12-31"),
+    "Lata 70": ("1970-01-01", "1979-12-31"),
+    "Lata 60": ("1960-01-01", "1969-12-31"),
+    "Lata 50+": ("1900-01-01", "1959-12-31"),
+}
